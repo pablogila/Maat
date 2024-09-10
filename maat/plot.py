825f9ce467ea9e2ae5cbf329c0dfc99411a7bbd3
Pablo Gila-Herranz <pablo.gila.herranz@gmail.com>
2024-09-10 16:58:26 +0200

    fixed plotting

--- maat/plot.py
+++ maat/plot.py
@@ -68,20 +68,20 @@ def spectra(spectrum:Spectra):
     
     if hasattr(sdata, 'plotting') and hasattr(sdata.plotting, 'legend') and sdata.plotting.legend is not None:
         if len(sdata.plotting.legend) == len(sdata.dataframe):
-            for i, df in enumerate(sdata.plotting.dataframe):
+            for i, df in enumerate(sdata.dataframe):
                 clean_name = sdata.plotting.legend[i]
                 df.plot(x=df.columns[0], y=df.columns[1], label=clean_name, ax=ax)
         elif len(sdata.plotting.legend) == 1:
             clean_name = sdata.plotting.legend[0]
-            for i, df in enumerate(sdata.plotting.dataframe):
+            for i, df in enumerate(sdata.dataframe):
                 df.plot(x=df.columns[0], y=df.columns[1], label=clean_name, ax=ax)
-    else:  # If no legend is provided, use the filename instead
-        for name in sdata.filename:
+    elif len(sdata.filename) == len(sdata.dataframe):
+        for df, name in zip(sdata.dataframe, sdata.filename)
             clean_name = name
             for string in strings_to_delete_from_name:
                 clean_name = clean_name.replace(string, '')
             clean_name = clean_name.replace('_', ' ')
-        df.plot(x=df.columns[0], y=df.columns[1], label=clean_name, ax=ax)
+            df.plot(x=df.columns[0], y=df.columns[1], label=clean_name, ax=ax)
 
         # TO-ADD: horizontal lines
 
