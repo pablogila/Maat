fixed plateau calculation

maat/deuteration.py
@@ -55,13 +55,13 @@ def impulse_approx(ins: Spectra,
     ratio = plateau_D_normalized / plateau_H_normalized  # ratio_ideal < ratio < 1.0
     ratio_ideal = material_D.cross_section / material_H.cross_section  # 0.0 < ratio_ideal < 1.0
 
-    ratio_error = ratio * np.sqrt((plateau_H_normalized_error / plateau_H_normalized)**2 + (plateau_D_normalized_error / plateau_D_normalized)**2)
+    ratio_error = abs(ratio * np.sqrt((plateau_H_normalized_error / plateau_H_normalized)**2 + (plateau_D_normalized_error / plateau_D_normalized)**2))
 
     deuteration = (1 - ratio) / (1 - ratio_ideal)
-    deuteration_error = deuteration * np.sqrt((ratio_error / ratio)**2)
+    deuteration_error = abs(deuteration * np.sqrt((ratio_error / ratio)**2))
 
-    print(f'Plateau H:                 {plateau_H_normalized} +- {plateau_H_normalized_error}')
-    print(f'Plateau D:                 {plateau_D_normalized} +- {plateau_D_normalized_error}')
+    print(f'Normalized plateau H:      {plateau_H_normalized} +- {plateau_H_normalized_error}')
+    print(f'Normalized plateau D:      {plateau_D_normalized} +- {plateau_D_normalized_error}')
     print(f'Ratio D/H plateaus:        {ratio} +- {ratio_error}')
     print(f'Ratio D/H cross sections:  {ratio_ideal}')
 

maat/fit.py
@@ -1,3 +1,4 @@
+from .constants import *
 from .classes import *
 import scipy
 import numpy as np
@@ -16,7 +17,14 @@ def plateau(spectra:Spectra, low_cut:float, top_cut:float, df_index:int=0):
     if top_cut is not None:
         df = df[df[df.columns[0]] <= top_cut]
     mean = df[df.columns[1]].mean()
-    std = df[df.columns[1]].std()
+    std_mean = df[df.columns[1]].std()
+    error_column = next((col for col in file_keys['Error'] if col in df.columns), None)  # Get the error column title
+    if error_column:
+        errors = df[error_column].to_numpy()
+        std_data = np.sqrt(np.sum(errors**2)) / len(errors)
+        std = np.sqrt(std_data**2 + std_mean**2)
+    else:
+        std = std_mean
     return mean, std
 
 
@@ -50,8 +58,9 @@ def area_under_peak(spectra:Spectra, peak:list, df_index:int=0, errors_as_in_bas
 
     area = scipy.integrate.simpson(y, x=x)
 
-    if 'Error' in df_range.columns:
-        point_errors = df_range['Error'].to_numpy()
+    error_column = next((col for col in file_keys['Error'] if col in df_range.columns), None)  # Get the error column title
+    if error_column:
+        point_errors = df_range[error_column].to_numpy()
     else: # Assume the error in each point is the same as the baseline error
         if errors_as_in_baseline == True:
             point_errors = np.full_like(y, baseline_error)